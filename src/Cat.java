45e90953ae492705d4d131353d2a11e471e3270a
Claudia <ycheng1@mines.edu>
2022-02-23 15:04:40 -0700

    Added whisker

--- src/Cat.java
+++ src/Cat.java
@@ -19,7 +19,11 @@ public class Cat {
 	// mouth starts about 40% from left edge of head
 	private static final int MOUTH_X = HEAD_DIMENSION/5 * 2;
 	private static final int MOUTH_Y = HEAD_DIMENSION/5 * 3;
-	
+	// whiskers are about 1/4 the size of the head
+	private static final int WHISKER_LENGTH = HEAD_DIMENSION/4;
+	private static final int WHISKER_SLANT = 5;
+	private static final int NUM_WHISKERS = 3;
+
 	// draw will render the Cat on the Graphics object
 	public void draw(Graphics g, int catX, int catY)
 	{
